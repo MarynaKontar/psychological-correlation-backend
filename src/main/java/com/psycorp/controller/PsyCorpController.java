002e54cc4564be94d4f82cda59dae838d96c40c2
Vitalii Proskura <vitalii.proskura@gmail.com>
2018-03-31 23:22:25 +0300

    initial commit

--- src/main/java/com/psycorp/controller/PsyCorpController.java
+++ src/main/java/com/psycorp/controller/PsyCorpController.java
@@ -2,13 +2,15 @@ package com.psycorp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class InitialController {
+@Controller
+@RequestMapping ("/")
+public class PsyCorpController {
 
-    @GetMapping
+    @RequestMapping (value = "")
     public String initial() {
-        return "Hello world!";
+        return "index";
     }
 }
